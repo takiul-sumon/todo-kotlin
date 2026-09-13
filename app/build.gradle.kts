0351a1c18da48be6f1e37dbb0fdc6773136a015c
Md Takiul Islam Sumon <takiulislam20@gmail.com>
2026-09-13 15:31:23 +0600

    ui done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui)
